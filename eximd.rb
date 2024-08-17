9a0e3b966ad5a02a792d607edde61f810abc02d6
Sam Uherek <samuherekbiz@gmail.com>
2024-08-17 16:54:29 +0200

    Update and rename eximed.rb to eximd.rb

--- eximd.rb
+++ eximd.rb
@@ -1,14 +1,14 @@
-class Eximed < Formula
+class Eximd < Formula
   desc "Quick exif medic for your media files"
   homepage "https://github.com/samuherek/eximed"
-  url "https://github.com/samuherek/eximed/releases/download/v0.1.4/eximed-v0.1.4-aarch64-apple-darwin.tar.gz"
+  url "https://github.com/samuherek/eximd/releases/download/v0.1.4/eximd-v0.1.4-aarch64-apple-darwin.tar.gz"
   sha256 "f6214bc6a77dd8bc75483eb6f04e459a7cd45a860de43a612eb7ad69d0092b98"
 
   def install
-    bin.install "eximed"
+    bin.install "eximd"
   end
 
   test do
-    system "#{bin}/eximed", "--version"
+    system "#{bin}/eximd", "--version"
   end
 end
