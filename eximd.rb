1f7c1c9c542501b924849df5cbf888cfd7008e33
Sam Uherek <samuherekbiz@gmail.com>
2024-08-18 09:46:13 +0200

    Update eximd.rb

--- eximd.rb
+++ eximd.rb
@@ -1,6 +1,6 @@
 class Eximd < Formula
   desc "Quick exif medic for your media files"
-  homepage "https://github.com/samuherek/eximed"
+  homepage "https://github.com/samuherek/eximd"
   url "https://github.com/samuherek/eximd/releases/download/cli-v0.1.10/eximd-v0.1.10-aarch64-apple-darwin.tar.gz"
   sha256 "03b7e788d41ca6e1dd94372e11d464897cf14278a0e83839308761a9765191d1"
 
